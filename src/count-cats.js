d16c5f81d90a5fbd3274d551ee7e5c646013b7cd
Toffifi <sanyq@mail.ru>
2020-02-25 18:35:44 +0300

    feat: count cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function countCats(matrix) {
+  let cats = 0;
+  for (let i in matrix) {
+    for (let j in matrix[i]) {
+      if(matrix[i][j] === '^^') {
+        cats += 1;
+      }
+    }
+  }
+  return cats;
+}
