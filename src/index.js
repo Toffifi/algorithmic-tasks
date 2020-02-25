8e4106062d1b7a0ff7e856bfdfe683783aafa3b8
Toffifi <sanyq@mail.ru>
2020-02-25 11:06:32 +0300

    fix: misprint

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 exports.min = function min (array) {
   if (!array || !array.length) {
     return 0;
-  } else if (!arguments || ! arguments.length) {
+  } else if (! arguments.length) {
     return 0;
   } else {
     return array.sort((a, b) => a - b)[0];
@@ -13,7 +13,7 @@ exports.min = function min (array) {
 exports.max = function max (array) {
   if (!array || !array.length) {
     return 0;
-  } else if (!arguments || ! arguments.length) {
+  } else if (! arguments.length) {
     return 0;
   } else {
     return array.sort((a, b) => b - a)[0];
@@ -24,7 +24,7 @@ exports.max = function max (array) {
 exports.avg = function avg (array) {
   if (!array || !array.length) {
     return 0;
-  } else if (!arguments || ! arguments.length) {
+  } else if (! arguments.length) {
     return 0;
   } else {
     return array.reduce((a, b) => (a + b)) / array.length
