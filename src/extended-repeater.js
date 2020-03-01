e53b4cc2bcaf1255913a92747372b4b0193b2ab3
Toffifi <sanyq@mail.ru>
2020-03-01 14:06:40 +0300

    fix: extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,9 +1,11 @@
 module.exports = function repeater(str, options = {}) {
     let string = '';
-    for (let i = 0; i < options.repeatTimes ? options.repeatTimes : 1; i++) {
+    for (let i = 0; i < (options.repeatTimes ? options.repeatTimes : 1); i++) {
         string += String(str);
-        for (let j = 0; j < options.additionRepeatTimes ? options.additionRepeatTimes : 1; j++) {
-            string += String(options.addition);
+        for (let j = 0; j < (options.additionRepeatTimes ? options.additionRepeatTimes : 1); j++) {
+            if (options.addition !== undefined) {
+                string += String(options.addition);
+            }
             if(j < options.additionRepeatTimes - 1) {
                 string += options.additionSeparator ? options.additionSeparator : '|';                
             }
