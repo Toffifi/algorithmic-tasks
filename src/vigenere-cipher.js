4371de44d73fa375c24b0f95a654db0b2e0af2bb
Toffifi <sanyq@mail.ru>
2020-03-01 19:56:07 +0300

    feat: vigenere cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,37 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(direction = true){
+        this.direction = direction;
+    }
+    encrypt(message, key) {
+        return this.worker(message, key, true);
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    decrypt(encryptedMessage, key) {
+        return this.worker(encryptedMessage, key, false);
+    }
+
+    worker (message, key, encrypt = true) {
+        let resultString = "";
+        let counter = 0;
+        const messageUpper = message.toUpperCase();
+        const keyUpper = key.toUpperCase();
+        for (let i = 0; i < message.length; i++) {
+            const code = messageUpper.charCodeAt(i) - 65;
+            if (code >= 0 && code <= 25) {
+                const keyCode = keyUpper.charCodeAt(counter % key.length) - 65;
+                let resultCode;
+                if (encrypt) {
+                    resultCode = (code + keyCode) <= 25 ? code + keyCode : (code + keyCode) - 26;
+                } else {
+                    resultCode = (code - keyCode)  >= 0 ? code - keyCode : (code - keyCode) + 26;
+                }
+                resultString += String.fromCharCode(resultCode + 65);
+                counter++;
+            } else {
+                resultString += message[i];
+            }
+        }
+        return this.direction ? resultString : resultString.split('').reverse().join('');
     }
 }
 
